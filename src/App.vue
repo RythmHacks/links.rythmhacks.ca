fd1bd6e6e9fa522f5d6830565e720dee086a575d
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-12-10 17:09:19 -0500

    add stuff

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,35 @@
 <template>
-  MONKE
+  <div className="card">
+    <div id="one">
+      <img src="./assets/logosmall.png" className="ml-auto mr-auto w-[40%] rounded-full"/>
+      <h1 className="text-7xl font-bold text-center mt-8">RythmHacks</h1>
+      <p className="text-[1.25rem] text-center mt-6">Here are some important links and resources for RythmHacks!</p>
+    </div>
+    <div id="two">
+      <a href="https://forms.gle/4Az622QCitExXvXx6" target="_blank" rel="noreferrer">
+        Apply to be an organizer!
+        <i class="bi bi-people"></i>
+      </a>
+      <a href="https://www.rythmhacks.ca/" target="_blank" rel="noreferrer">
+        Check out our cool website!
+        <i class="bi bi-arrow-up-right-square"></i>
+      </a>
+      <a href="https://www.instagram.com/rythm.hacks/" target="_blank" rel="noreferrer">
+        Instagram
+        <i class="bi bi-instagram"></i>
+      </a>
+      <a href="https://www.linkedin.com/company/rythmhacks/" target="_blank" rel="noreferrer">
+        LinkedIn
+        <i class="bi bi-linkedin"></i>
+      </a>
+      <a href="https://twitter.com/rythmhacks" target="_blank" rel="noreferrer">
+        Twitter
+        <i class="bi bi-twitter"></i>
+      </a>
+      <a href="https://github.com/RythmHacks" target="_blank" rel="noreferrer">
+        GitHub
+        <i class="bi bi-github"></i>
+      </a>
+    </div>
+  </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'accent': '#548DA9'
+      }
+    },
+  },
+  plugins: [],
+}
