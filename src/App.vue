c5f1ace7a6bf80aa12bc16dc2662477b07c47d9c
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-12-20 20:55:06 -0500

    fix stuff

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
 export default {
   data() {},
   methods: {
-    onClick() {
-      window.open("https://www.rythmhacks.ca/", "_blank");
+    onClick(link) {
+      window.open(link, "_blank");
     },
     }
 }
@@ -12,32 +12,32 @@ export default {
 <template>
   <div className="card">
     <div id="one">
-      <img src="./assets/logosmall.png" className="ml-auto mr-auto w-[40%] rounded-full cursor-pointer hover:scale-105 duration-300" @click="onClick"/>
+      <img src="./assets/logosmall.png" className="ml-auto mr-auto w-[40%] rounded-full cursor-pointer hover:scale-105 duration-300" @click="onClick('rythmhacks.ca')"/>
       <h1 className="font-bold text-center mt-8">RythmHacks</h1>
       <p className="text-[1.25rem] text-center mt-6">Here are some important links and resources for RythmHacks!</p>
     </div>
     <div id="two">
-      <button href="https://forms.gle/4Az622QCitExXvXx6" target="_blank" rel="noreferrer">
+      <button @click="onClick('https://forms.gle/4Az622QCitExXvXx6')">
         Apply to be an organizer!
         <i class="bi bi-people"></i>
       </button>
-      <button href="https://www.rythmhacks.ca/" target="_blank" rel="noreferrer">
+      <button @click="onClick('https://www.rythmhacks.ca/')">
         Check out our cool website!
         <i class="bi bi-arrow-up-right-square"></i>
       </button>
-      <button href="https://www.instagram.com/rythm.hacks/" target="_blank" rel="noreferrer">
+      <button @click="onClick('https://www.instagram.com/rythm.hacks/')">
         Instagram
         <i class="bi bi-instagram"></i>
       </button>
-      <button href="https://www.linkedin.com/company/rythmhacks/" target="_blank" rel="noreferrer">
+      <button @click="onClick('https://www.linkedin.com/company/rythmhacks/')">
         LinkedIn
         <i class="bi bi-linkedin"></i>
       </button>
-      <button href="https://twitter.com/rythmhacks" target="_blank" rel="noreferrer">
+      <button @click="onClick('https://twitter.com/rythmhacks/')">
         Twitter
         <i class="bi bi-twitter"></i>
       </button>
-      <button href="https://github.com/RythmHacks" target="_blank" rel="noreferrer">
+      <button @click="onClick('https://github.com/RythmHacks')">
         GitHub
         <i class="bi bi-github"></i>
       </button>
